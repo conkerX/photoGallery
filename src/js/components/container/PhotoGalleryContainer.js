5b1b9a8600ae05f0a9c9d487157e7f6480fe6d5f
Brandon Hadler <brandon.hadler30@gmail.com>
2018-11-09 20:51:24 -0800

    allow .scss and .css

--- src/js/components/container/PhotoGalleryContainer.js
+++ src/js/components/container/PhotoGalleryContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Test from "../presentational/Test.jsx";
+// import "../../../styles/appStyles.scss";
+import "../../../styles/appStyles.css";
 
 class PhotoGalleryContainer extends Component {
   constructor() {
@@ -13,6 +15,8 @@ class PhotoGalleryContainer extends Component {
   render() {
     return (
       <div>
+        <h1>Hello from h1</h1>
+        <h2>Hello from h2</h2>
         <Test world={this.state.world} />
       </div>
     );
